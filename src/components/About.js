0d090c3a1d9a643c24453df1024f2e6420b9cdc8
khalid ibrahim <ibrahim.khalid4778@gmail.com>
2020-11-14 18:19:25 +0000

    finished the about me page css

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,90 @@
 import React from 'react'
-import {fa} from '@fortawesome/free-solid-svg-icons'
 import {SiJavascript, SiHtml5, SiReact, SiNodeDotJs, SiCss3, SiSass, SiFirebase, SiNginx} from 'react-icons/si'
+import {FaPencilRuler, FaFileCode} from 'react-icons/fa'
+import './css/main.css'
 function About (){
     return (
         <div className="about">
             <div className="about__container">
                 <div className="about__title">
-                    <h3>About us</h3>
+                    <h3>About me</h3>
                     <h1>WHO AM I? </h1>
                 </div>
                 <div className="about__content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, beatae earum nemo laborum velit provident.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam magnam molestiae assumenda sapiente, eos aperiam.</p>
+                    <p>I am junior full stack developer and UX/UI designer. I create beautful & functional websites. Feel free to have a look at some of my projects!</p>
+                    
                 </div>
-                <div className="about__skill__ShowCase">
-                    <SiReact />
-                    <SiJavascript />
-                    <SiHtml5 />
-                    <SiCss3 />
-                    <SiSass />
-                    <SiFirebase />
-                    <SiNginx />
-                    <SiNodeDotJs/>
+
+                <div className="about__skills__showCase">
+                    <div className="about__design">
+                        <div className="about__design__logo">
+                            <FaPencilRuler />
+                        </div>
+                        <div className="about__design__title">
+                            <h1>Designer</h1>
+                            <p>I value simple content structure, clean design pattern, and thoughtful interactions.</p>
+                        </div>
+                        <div className="about__design__content">
+                            <h1>Things i enjoy designing</h1>
+                            <p>UX, UI, WEB, MOBILE,LOGOS</p>
+                        </div>
+                        <div className="about__design__tools">
+                            <h1>design tools</h1>
+                            <ul>
+                                <li>Figma</li>
+                                <li>sketch</li>
+                                <li>adobe xd</li>
+                                <li>photoshop</li>
+                                <li>Invation</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="about__code">
+                        <div className="about__design__logo">
+                            <FaFileCode />
+                        </div>
+                        <div className="about__design__title">
+                            <h1>full stack developer</h1>
+                            <p>I like to code from scratch and enjoy bringing ideas to life in the browser.</p>
+                        </div>
+                        <div className="about__design__content">
+                            <h1>Techs i know</h1>
+                            <div className="content__div">
+                                <p>html</p>
+                                <p>css</p>
+                                <p>sass</p>
+                                <p>material-ui</p>
+                                <p>javaScript</p>
+                                <p>react</p>
+                                <p>nodeJs</p>
+                                <p>nginx</p>
+                            </div>
+                        </div>
+                        <div className="about__design__tools">
+                            <h1>dev tools</h1>
+                            <ul>
+                                <li>Visual studio</li>
+                                <li>sublime</li>
+                                <li>terminal</li>
+                                <li>linux</li>
+                                <li>github</li>
+                                <li>gitlao</li>
+                                <li>firebase</li>
+                                <li>digitalocean</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="about__icons">
+                    <SiReact className="about__skill__icons react" />
+                    <SiJavascript className="about__skill__icons js"  />
+                    <SiHtml5 className="about__skill__icons html" />
+                    <SiCss3 className="about__skill__icons css" />
+                    <SiSass className="about__skill__icons sass" />
+                    <SiFirebase className="about__skill__icons fb"  />
+                    <SiNginx className="about__skill__icons ngx" />
+                    <SiNodeDotJs className="about__skill__icons nodejs" />
                 </div>
             </div>
         </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,36 +13,31 @@ function Nav() {
             <div className="nav__container">
                 <div className="nav__title">
                     <h1><span>khalid</span> ibrahim</h1>
-                    <h3>full stack developer</h3>
+                    <h3>full stack developer and designer</h3>
                 </div>
                 <div className="nav__links">
                     <ul>
                         <Link className="nav__links__tag" to="/"><li>home</li></Link>
                         <Link className="nav__links__tag" to="/about"><li>about</li></Link>
                         <Link className="nav__links__tag" to="/projects"><li>portfolio</li></Link>
-                        <Link className="nav__links__tag" to="/contact"><li>contact</li></Link>
                     </ul>
                 </div>
                 <div className="nav__socials">
-                    <div>
-                        <LinkedIn className="icons li" /> 
-                        <h3>LinkedIn</h3> 
+                    <div className="nav__socials__icons">
+                        <a href={"https://www.linkedin.com/"} target="_blank"><span> <LinkedIn className="icons li" /> </span></a>
+                        <a href={'https://github.com/bigkay-designer/'} target="_blank"><span> <GitHub className="icons gh" />  </span></a>
+                        <a href={"https://mail.google.com/mail/u/0/#inbox?compose=new"} target="_blank"><span> <MailOutline className="icons em" /> </span></a>
+                        <a href={"#"} target="_blank"><span> <Description className="icons desc" />   </span></a>
+                         
                     </div>
-                    <div>
-                        <GitHub className="icons gh" /> 
-                        <h3>GitHub</h3> 
-                    </div>
-                    <div>
-                        <MailOutline className="icons em" /> 
-                        <h3>Email</h3> 
-                    </div>
-                    <div>
-                        <Description className="icons desc" /> 
-                        <h3>Resume</h3> 
-                    </div>                   
                 </div>
                 <div className="footer">
                     <footer className="footer__container">
+                        <div className="nav__contact">
+                            <h2>Any questions don't hesitate to contact me</h2>
+                            <p><a href="https://mail.google.com/mail/u/0/#inbox?compose=new" target="_blank">ibrahimkhalid478@gmail.com</a></p>
+                            <p>07494498323</p>
+                        </div>
                         <h4> © 2020 Khalid Ibrahim. All Rights Reserved.</h4>
                     </footer>
                 </div>
