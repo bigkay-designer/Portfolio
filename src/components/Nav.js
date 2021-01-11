327a037f7991e58b06d5f915d65766a7080f61ae
Khalid Ibrahim <khalidibrahim@khalids-mbp.mynet>
2021-01-11 21:50:54 +0000

    updated resume link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -38,7 +38,7 @@ function Nav() {
                             <a href={"https://www.linkedin.com/in/khalid-ibrahim-3b2b71201"} target="_blank" rel="noreferrer"><span> <LinkedIn className="icons li" /> </span> <span className="nav__icons__title">LinkedIn</span> </a>
                             <a href={'https://github.com/bigkay-designer/'} target="_blank" rel="noreferrer"><span> <GitHub className="icons gh" />  </span> <span className="nav__icons__title">GitHub</span> </a>
                             <a href={"https://mail.google.com/mail/u/0/#inbox?compose=new"} target="_blank" rel="noreferrer"><span> <MailOutline className="icons em" /> </span> <span className="nav__icons__title">Email</span></a>
-                            <a href={"https://drive.google.com/file/d/1szQUGlUMihFLuhLDsjDv8HOG1whwcOAg/view?usp=sharing"} target="_blank" rel="noreferrer"><span> <Description className="icons desc" /> </span> <span className="nav__icons__title">Resume</span> </a>
+                            <a href={"https://drive.google.com/file/d/142DzFIjNzHXdDmg94gl5Rn7xtQaNYvXK/view?usp=sharing"} target="_blank" rel="noreferrer"><span> <Description className="icons desc" /> </span> <span className="nav__icons__title">Resume</span> </a>
                             
                         </div>
                     </div>
