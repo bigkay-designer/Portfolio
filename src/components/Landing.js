63405cb2b377e62d1ee796d5a09b3c91c7850ab5
khalid ibrahim <ibrahimkhalid478@gmail.com>
2021-09-01 07:50:39 +0100

    edited the modal json file

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import About from './About';
 import Projects from './Projects';
+import './css/landing.css';
 function Landing() {
   return (
-    <div>
+    <div className="landing">
       <Projects />
       <About />
     </div>
